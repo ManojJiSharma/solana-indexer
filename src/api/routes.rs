8b50c78cf6b4b3cf5122e3e41071cd0d774d9784
ManojJiSharma <manoj.sharma.bit.india@gmail.com>
2026-01-09 15:51:26 +0530

    fix for slot

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -7,5 +7,5 @@ pub fn routers() -> Router<AppState>{
     Router::new()
         .route("/health", get(handlers::health))
         .route("/tx/{signature}", get(handlers::get_tx))
-        .route("/slot/:slot/txs", get(handlers::get_slot_txs))
+        .route("/slot/{slot}/txs", get(handlers::get_slot_txs))
 }
